73f64404b1d2c44f2a7a4988cc1c47cb119287b8
soelz4-nixos <soheilzolfaghari78@gmail.com>
2026-07-06 13:44:04 +0330

    feat: .nix folder added

---
+++ .nix/shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.python314
+    pkgs.libxml2
+    pkgs.libxslt
+    pkgs.pkg-config
+    pkgs.zlib
+    pkgs.gcc
+    pkgs.stdenv.cc.cc.lib
+  ];
+
+  shellHook = ''
+    echo "Python 3.14 Development Shell"
+
+    export LD_LIBRARY_PATH=${
+      pkgs.lib.makeLibraryPath [
+        pkgs.libxml2
+        pkgs.libxslt
+        pkgs.zlib
+        pkgs.stdenv.cc.cc.lib
+      ]
+    }:$LD_LIBRARY_PATH
+  '';
+}
